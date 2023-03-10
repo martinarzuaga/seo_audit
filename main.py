2d232a1c73e61f9b5c989d90bc49cec0e5855f84
Martin Arzuaga <martinarzuagafe@gmail.com>
2023-03-09 22:17:25 -0300

    last changes added

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import modules.update_cache as uc
 import modules.update_code_ratio as ucr
 import modules.update_web_vitals as uwv
 import modules.update_pagespeedinsights as psi
+import modules.update_mobile_friendly_test as mft
 
 lgs.log_to_gs()
 
 mobile_results = rt.request_tests_mobile(gu.urls_list)
 desktop_results = rt.request_tests_desktop(gu.urls_list)
 
+# ALL TEST
 ud.update_velocidad_mobile(mobile_results, gu.wks_velocidad)
 ud.update_velocidad_desktop(desktop_results, gu.wks_velocidad)
 ucr.update_code_ratio(mobile_results, gu.wks_velocidad)
@@ -21,5 +23,7 @@ uc.update_cache(mobile_results, gu.wks_cache)
 uwv.update_web_vitals_mobile(mobile_results, gu.wks_webVitals)
 uwv.update_web_vitals_desktop(desktop_results, gu.wks_webVitals)
 
-psi.update_psi_mobile(mobile_results, gu.wks_pageSpeed)
-psi.update_psi_desktop(desktop_results, gu.wks_pageSpeed)
+psi.update_psi_mobile(gu.urls_list, gu.wks_pageSpeed)
+psi.update_psi_desktop(gu.urls_list, gu.wks_pageSpeed)
+
+mft.update_mobileFriendlyTest()
---
+++ modules/get_urls.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import credentials.loging_to_gs as lgs
+
+gc = lgs.log_to_gs()
+
+# open the Google spreadsheet and save all the sheets
+sh = gc.open('Auditoria-Performance-Divino')
+
+# save all the worksheets in variables
+wks_velocidad = sh.worksheet('title', 'Velocidad')
+wks_cache = sh.worksheet('title', 'Caché')
+wks_webVitals = sh.worksheet('title', 'Web Vitals')
+wks_pageSpeed = sh.worksheet('title', 'PageSpeed')
+
+# save the worksheets also un data frames
+df_velocidad = pd.DataFrame(wks_velocidad)
+df_cache = pd.DataFrame(wks_cache)
+df_webVitals = pd.DataFrame(wks_webVitals)
+df_pageSpeed = pd.DataFrame(wks_pageSpeed)
+
+# save all the URLs to check to
+urls_list = []
+
+for url in wks_velocidad.range('B2:B100'):
+    if 'http' in url[0].value:
+        urls_list.append(url[0].value)
