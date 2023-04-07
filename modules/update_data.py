fd47eae44b3e85d2acdea59e2d6183e271e54797
Martin Arzuaga <martinarzuagafe@gmail.com>
2023-04-07 04:01:09 -0300

    programa funcionando

---
+++ modules/update_data.py
@@ -0,0 +1,37 @@
+from modules.get_urls import *
+from modules.request_tests import *
+
+
+def update_velocidad(data):
+
+    for i in range(len(data)):
+
+        speed_index = round(int(data[i][1]['data']['average']['firstView']['SpeedIndex']) / 1000)
+
+        sh.worksheet('title', 'Velocidad').update_value(f'C{i+2}', speed_index)
+
+
+def update_cache(data):
+
+    for i in range(len(data)):
+        # Add KB first and repeat view to the spreadsheet
+        kb_first_view = round(int(
+            data[i][1]['data']['average']['firstView']['bytesIn']) / 1024)
+
+        wks_cache.update_value(f'C{i + 2}', kb_first_view)
+
+        kb_repeat_view = round(int(
+            data[i][1]['data']['average']['repeatView']['bytesIn']) / 1024)
+
+        wks_cache.update_value(f'D{i + 2}', kb_repeat_view)
+
+        # Add REQUESTS first and repeat view to the spreadsheet
+        requests_first_view = int(
+            data[i][1]['data']['average']['firstView']['requestsFull'])
+
+        wks_cache.update_value(f'F{i + 2}', requests_first_view)
+
+        requests_repeat_view = int(
+            data[i][1]['data']['average']['repeatView']['requestsFull'])
+
+        wks_cache.update_value(f'G{i + 2}', requests_repeat_view)
--- modules/update_pagespeedinsights.py
+++ modules/update_pagespeedinsights.py
@@ -23,11 +23,13 @@ def update_psi_mobile(urls, wks_pagespeed):
     """
 
     for i in range(len(urls)):
-        endpoint = url_test_endpoint + 'url=' + urls[i][0] + sep + mobile_strategy + sep + locale + sep + psi_api_key
+        print(f'Analizando {urls[i]}')
+        endpoint = url_test_endpoint + 'url=' + urls[i] + sep + mobile_strategy + sep + locale + sep + psi_api_key
         response = urllib.request.urlopen(endpoint)
         data = json.loads(response.read())
         overall_score = data["lighthouseResult"]["categories"]["performance"]["score"] * 100
         wks_pagespeed.update_value(f'C{i+2}', overall_score)
+        print(f'{urls[i]} actualizada')
     
     print('PSI Score Mobile Updated')
 
@@ -42,10 +44,12 @@ def update_psi_desktop(urls, wks_pagespeed):
     """
     
     for i in range(len(urls)):
-        endpoint = url_test_endpoint + 'url=' + urls[i][0] + sep + desktop_strategy + sep + locale + sep + psi_api_key
+        print(f'Analizando {urls[i]}')
+        endpoint = url_test_endpoint + 'url=' + urls[i] + sep + desktop_strategy + sep + locale + sep + psi_api_key
         response = urllib.request.urlopen(endpoint)
         data = json.loads(response.read())
         overall_score = data["lighthouseResult"]["categories"]["performance"]["score"] * 100
         wks_pagespeed.update_value(f'D{i+2}', overall_score)
+        print(f'{urls[i]} actualizada')
     
     print('PSI Score Desktop Updated')
