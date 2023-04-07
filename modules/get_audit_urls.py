4d3b8a72942a5c98ab4fd297db4aa072756ab0ca
Martin Arzuaga <martinarzuagafe@gmail.com>
2023-04-07 04:35:07 -0300

    ultimo intento de merging

--- modules/get_audit_urls.py
+++ modules/get_audit_urls.py
@@ -1,29 +1,37 @@
 import pandas as pd
 import pygsheets
+import requests
 from modules.create_endpoint import client_name
 
 # Login to Google Sheets
-gc = pygsheets.authorize(client_secret='./credentials/client_secret.json')
+gc = pygsheets.authorize(client_secret='./credentials/client_secret_martin.json')
 
 # open the Google spreadsheet and save all the sheets
-sh = gc.open(f'Auditoria-SEO-{client_name}')
+# sh = gc.open(f'Auditoria-SEO-{client_name}')
+# Activate this line below when you are doing a review
+# sh = gc.open(f'Auditoria-SEO-{client_name}-Revision')
+# Activate this line below when you are doing another task
+sh = gc.open(f'Auditoria-Performance-{client_name}')
+# Activate this line below when you are doing another task
+# sh = gc.open(f'Auditoria-Performance-{client_name}-Revision')
 
 # save all the workseets in variables
 wks_velocidad = sh.worksheet('title', 'Velocidad')
 wks_cache = sh.worksheet('title', 'Caché')
 wks_webVitals = sh.worksheet('title', 'Web Vitals')
 wks_pageSpeed = sh.worksheet('title', 'PageSpeed')
-wks_webVitals = sh.worksheet('title', 'Web Vitals')
-
-# save the worksheets also un data frames
-df_velocidad = pd.DataFrame(wks_velocidad)
-df_cache = pd.DataFrame(wks_cache)
-df_webVitals = pd.DataFrame(wks_webVitals)
-df_pageSpeed = pd.DataFrame(wks_pageSpeed)
+# wks_mobileFriendly = sh.worksheet('title', 'Web Responsive')
 
 # save all the URLs to check to
 urls_list = []
 
-for url in wks_velocidad.range('B2:B100'):
-    if 'http' in url[0].value:
-        urls_list.append(url[0].value)
+for row in wks_velocidad.range('B2:B100'):
+    if 'http' in row[0].value:
+        url = row[0].value
+        request = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"})
+        if request.status_code == 200:
+            urls_list.append(row[0].value)
+        else:
+            continue
+    else:
+        break
