2f48c950c74c7b47fb32ba68d49ce475e79fb3fd
Martin Arzuaga <martinarzuagafe@gmail.com>
2023-09-12 16:29:05 -0300

    toda la auditoria completa - falta agregar excepciones a los alt en imagenes

--- modules/meta_tags.py
+++ modules/meta_tags.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import pygsheets
 from modules.create_endpoint import client_name
+from urllib.parse import urlparse
 
 ## Setup chrome options
 options = Options()
@@ -35,6 +36,15 @@ def get_rendered_source_code(url):
 gc = pygsheets.authorize(client_secret='./credentials/client_secret_martin.json')
 sh = gc.open(f'Auditoria-SEO-{client_name}')
 
+file = open('./configuracion.txt', 'r')
+file = file.readlines()
+domain = file[2].replace('\n', '')
+try:
+    cdn = file[3].replace('\n', '')
+except:
+    cdn = None
+
+
 def get_audit_sheet(gc):
     sh = gc.open(f'Auditoria-SEO-{client_name}')
     return sh
@@ -47,10 +57,8 @@ def seo_title(html, sheet, cell_number):
     try:
         title = html.find('title').get_text()
         wks_titles.update_value(f'C{cell_number+2}', title)
-        print(f'Title de la celda E{cell_number + 2} actualizado')
     except:
         wks_titles.update_value(f'C{cell_number+2}', '')
-        print(f'Title de la celda E{cell_number + 2} actualizado')
 
 
 # Get the meta description and update it
@@ -61,10 +69,8 @@ def seo_meta_description(html, sheet, cell_number):
     try:
         meta_description = html.find('meta', {'name':'description'}).get('content')
         wks_desc.update_value(f'C{cell_number+2}', meta_description)
-        print(f'Meta-description de la celda E{cell_number + 2} actualizada')
     except:
         wks_desc.update_value(f'C{cell_number+2}', '')
-        print(f'Meta-description de la celda E{cell_number + 2} actualizada')
 
 
 # Get the meta robots value, update it and evaluate.
@@ -81,25 +87,22 @@ def seo_noindex_tag(html, sheet, cell_number):
         elif "index" in meta_robots_tag:
             wks_indexacion.update_value(f'C{cell_number+2}', 'Si')
             wks_indexacion.update_value(f'D{cell_number+2}', 'Sin bloqueo')
-            print(f'Meta robots tag de la celda E{cell_number + 2} actualizada')
     else:
         wks_indexacion.update_value(f'C{cell_number + 2}', 'Si')
         wks_indexacion.update_value(f'D{cell_number + 2}', 'Sin bloqueo')
-        print(f'Meta robots tag de la celda E{cell_number + 2} actualizada')
 
 def seo_canonical_tag(html, sheet, cell_number):
     # Get the Canonical Worksheet
     wks_canonicals = sheet.worksheet('title', 'URL Canonicals')
 
     meta_canonical_tag = html.find('link', {'rel':'canonical'})['href']
+
     if len(meta_canonical_tag) > 0:
         wks_canonicals.update_value(f'C{cell_number+2}', 'Configurado')
         wks_canonicals.update_value(f'D{cell_number+2}', meta_canonical_tag)
-        print(f'Canonical de la celda E{cell_number+2} actualizada')
     else:
         wks_canonicals.update_value(f'C{cell_number+2}', 'No Configurado')
         wks_canonicals.update_value(f'D{cell_number + 2}', '')
-        print(f'Canonical de la celda E{cell_number+2} actualizada')
 
 def seo_headings_tags(html, sheet, cell_number):
     # Get the headings Worksheet
@@ -125,20 +128,34 @@ def seo_headings_tags(html, sheet, cell_number):
 
     if len(h1_tags) >= 1:
         wks_headings.update_value(f'D{cell_number+2}', h1_values)
-        print(f'H1 de la celda D{cell_number + 2} actualizadas')
     elif len(h1_tags) == 0:
         wks_headings.update_value(f'D{cell_number + 2}', '')
-        print(f'H1 de la celda D{cell_number + 2} actualizadas')
 
     if len(h2_tags) > 0:
         wks_headings.update_value(f'E{cell_number + 2}', h2_values)
-        print(f'H2 de la celda E{cell_number + 2} actualizadas')
     elif len(h2_tags) == 0:
         wks_headings.update_value(f'E{cell_number + 2}', '')
-        print(f'H2 de la celda D{cell_number + 2} actualizadas')
 
     wks_headings.update_value(f'C{cell_number+2}', f'H1 ({len(h1_tags)}), H2 ({len(h2_tags)}), H3 ({len(h3_tags)}), H4 ({len(h4_tags)}), H5 ({len(h5_tags)}), H6 ({len(h6_tags)})')
 
+
+def seo_alt_images(html, sheet, cell_number):
+    # Get the headings Worksheet
+    wks_imagenes = sheet.worksheet('title', 'Optimización Imágenes')
+
+    img_tags = html.find_all('img')
+
+    img_total = len(img_tags)
+
+    img_alt = 0
+
+    for imagen in img_tags:
+        alt = 'alt'
+        if alt in imagen.attrs:
+            img_alt += 1
+
+    wks_imagenes.update_value(f'C{cell_number+2}', img_total)
+    wks_imagenes.update_value(f'D{cell_number+2}', img_total - img_alt)
 
 
 def run_meta_tags_test(urls_list, sheet):
@@ -149,15 +166,25 @@ def run_meta_tags_test(urls_list, sheet):
 
         # Get the title and update it
         seo_title(url, sheet, i)
+        print(f'Title de la celda E{i + 2} actualizado')
 
         # Get the meta description and updated
         seo_meta_description(url, sheet, i)
+        print(f'Meta-description de la celda E{i + 2} actualizada')
 
         # Evaluate the meta_robots_tag
         seo_noindex_tag(url, sheet, i)
+        print(f'Meta robots tag de la celda E{i + 2} actualizada')
 
         # Evaluate the canonical tag
         seo_canonical_tag(url, sheet, i)
+        print(f'Canonical de la celda E{i + 2} actualizada')
 
         # Update the H2 tags
         seo_headings_tags(url, sheet, i)
+        print(f'H1 de la celda D{i + 2} actualizadas')
+        print(f'H2 de la celda E{i + 2} actualizadas')
+
+        # Actualizar Optimización de Imágenes
+        seo_alt_images(url, sheet, i)
+        print(f'Alt en imágenes en la fila {i + 2} actualizado')
