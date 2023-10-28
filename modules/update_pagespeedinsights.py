f6b5840f55744a2c6faa92a22a00cd8e632b5cea
Martin Arzuaga <martinarzuagafe@gmail.com>
2023-10-28 16:52:13 -0300

    refactoring

--- modules/update_pagespeedinsights.py
+++ modules/update_pagespeedinsights.py
@@ -1,4 +1,5 @@
-import urllib.request, json
+import urllib.request
+import json
 
 psi_api_key = 'key=AIzaSyAYCP7iSxwNwA9qqdLUZ9X2BpPtA1L0rUY'
 
@@ -25,12 +26,13 @@ def update_psi_mobile(urls, wks_pagespeed):
     """
 
     for i in range(len(urls)):
-        endpoint = url_test_endpoint + 'url=' + urls[i] + sep + mobile_strategy + sep + locale + sep + psi_api_key
+        endpoint = url_test_endpoint + 'url=' + \
+            urls[i] + sep + mobile_strategy + sep + locale + sep + psi_api_key
         response = urllib.request.urlopen(endpoint)
         data = json.loads(response.read())
         overall_score = data["lighthouseResult"]["categories"]["performance"]["score"] * 100
         wks_pagespeed.update_value(f'C{i+2}', overall_score)
-    
+
     print('PSI Score Mobile Updated')
 
 
@@ -42,12 +44,13 @@ def update_psi_desktop(urls, wks_pagespeed):
         data (list): the result from get_urls. JSON response in a list format
         wks_velocidad (Worksheet): the Worksheet object to update.
     """
-    
+
     for i in range(len(urls)):
-        endpoint = url_test_endpoint + 'url=' + urls[i] + sep + desktop_strategy + sep + locale + sep + psi_api_key
+        endpoint = url_test_endpoint + 'url=' + \
+            urls[i] + sep + desktop_strategy + sep + locale + sep + psi_api_key
         response = urllib.request.urlopen(endpoint)
         data = json.loads(response.read())
         overall_score = data["lighthouseResult"]["categories"]["performance"]["score"] * 100
         wks_pagespeed.update_value(f'D{i+2}', overall_score)
-    
+
     print('PSI Score Desktop Updated')
--- modules/update_web_vitals.py
+++ modules/update_web_vitals.py
@@ -5,20 +5,22 @@ def update_web_vitals_mobile(mobile_data, wks_webVitals):
     Args:
         data (list): the result from get_urls. JSON response in a list format
         wks_webVitals (Worksheet): the Worksheet object to update.
-    """    
-    
+    """
+
     for i in range(len(mobile_data)):
-        
-        lcp = round(int(mobile_data[i][1]['data']['average']['firstView']['chromeUserTiming.LargestContentfulPaint']) / 1000, 2)
-        cls = round(float(mobile_data[i][1]['data']['average']['firstView']['chromeUserTiming.CumulativeLayoutShift']), 5)
-        tbt = int(mobile_data[i][1]['data']['average']['firstView']['TotalBlockingTime'])
-        
+
+        lcp = round(int(mobile_data[i][1]['data']['average']['firstView']
+                    ['chromeUserTiming.LargestContentfulPaint']) / 1000, 2)
+        cls = round(float(mobile_data[i][1]['data']['average']
+                    ['firstView']['chromeUserTiming.CumulativeLayoutShift']), 5)
+        tbt = int(mobile_data[i][1]['data']['average']
+                  ['firstView']['TotalBlockingTime'])
+
         wks_webVitals.update_value(f'C{i+2}', lcp)
         wks_webVitals.update_value(f'D{i+2}', cls)
         wks_webVitals.update_value(f'E{i+2}', tbt)
-    
+
     print('Web Core Vitals Mobile Updated')
-        
 
 
 def update_web_vitals_desktop(desktop_data, wks_webVitals):
@@ -27,14 +29,17 @@ def update_web_vitals_desktop(desktop_data, wks_webVitals):
     Args:
         data (list): the result from get_urls. JSON response in a list format
         wks_webVitals (Worksheet): the Worksheet object to update.
-    """    
-    
+    """
+
     for i in range(len(desktop_data)):
-        
-        lcp = round(int(desktop_data[i][1]['data']['average']['firstView']['chromeUserTiming.LargestContentfulPaint']) / 1000, 2)
-        cls = round(float(desktop_data[i][1]['data']['average']['firstView']['chromeUserTiming.CumulativeLayoutShift']), 5)
-        tbt = int(desktop_data[i][1]['data']['average']['firstView']['TotalBlockingTime'])
-        
+
+        lcp = round(int(desktop_data[i][1]['data']['average']['firstView']
+                    ['chromeUserTiming.LargestContentfulPaint']) / 1000, 2)
+        cls = round(float(desktop_data[i][1]['data']['average']
+                    ['firstView']['chromeUserTiming.CumulativeLayoutShift']), 5)
+        tbt = int(desktop_data[i][1]['data']['average']
+                  ['firstView']['TotalBlockingTime'])
+
         wks_webVitals.update_value(f'G{i+2}', lcp)
         wks_webVitals.update_value(f'H{i+2}', cls)
         wks_webVitals.update_value(f'I{i+2}', tbt)
